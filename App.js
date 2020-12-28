d2129cb8860f5871bf4a955fc6f2f2cd417bdc9f
pauladarias <pauladarias@gmail.com>
2020-12-28 08:10:09 +0000

    Set up the app

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from "react-native"
+import SearchScreen from "./src/screen(SearchScreen"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const navigator = createStackNavigator ({
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  Search: SearchScreen
+
+}, {
+  initialRouteName: "Search",
+  defaultNavigationOptions: {
+    title: "PubMate"
+  }
+})
+
+export default createAppContainer(navigator)
